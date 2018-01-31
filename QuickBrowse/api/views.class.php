90f41d389ea9b7b48d7353804130b5e9f1dfb391
DaDavinci <danny09vand@gmail.com>
2018-01-31 19:20:46 +0100

    Update views.class.php

--- QuickBrowse/api/views.class.php
+++ QuickBrowse/api/views.class.php
@@ -13,59 +13,13 @@ class view{
 		//What page should be displayed.
 		$file = strtok( basename($_SERVER["REQUEST_URI"]) , '?');
 		switch($file){
-			
-			case 'home':
-				self::$page = 'home.php';
-			break;
-			
-			case 'list':
-				self::$page = 'list.php';
-			break;
-			
-			case 'logout':
-				self::$page = 'logout.php';
-			break;
-			
-			case 'login':
-				self::$page = 'login.php';
-			break;
-			
-			case 'panel':
-				self::$page = 'panel.php';
-			break;
-			
-			case 'about':
-				self::$page = 'about.php';
-			break;
-			
-			case 'contact':
-				self::$page = 'contact.php';
-			break;
-			
-			case 'register':
-				self::$page = 'register.php';
-			break;
-			
-			case 'page':
-				self::$page = 'page.php';
-			break;
-			
-			case 'post':
-				self::$page = 'post.php';
-			break;
-			
-			case 'index':
-				self::$page = 'index.php';
-			break;
-			
+
 			case '':
 				self::$page = 'index.php';
 			break;
 			
 			default:
-				
 				self::$page = $file . '.php';
-				
 			break;
 			
 		}
