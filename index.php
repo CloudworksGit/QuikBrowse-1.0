23a4625ec413feb66a6b27fd04e3214ea7b4f806
DaDavinci <danny09vand@gmail.com>
2018-01-31 19:14:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	
-	// QUICKBROWSE IS PROPERTY OF DANNY V. DOOIJWEERT @ DVAND.NL //
+	// QUICKBROWSE IS PROPERTY OF DANNY V. DOOIJEWEERT @ DoOnline.nl //
 	ini_set('error_reporting', E_ALL);
 	include_once('./QuickBrowse/loader.php');
 	
